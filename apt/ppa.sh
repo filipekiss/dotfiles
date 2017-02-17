105fc68d81a91118ad55809fd6fc1ac3a09983f0
Filipe Kiss <eu@filipekiss.com.br>
2017-02-17 09:27:29 -0200

    Add Spotify to PPA

--- apt/ppa.sh
+++ apt/ppa.sh
@@ -4,16 +4,26 @@ DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$DIR" ]]; then DIR="$PWD"; fi
 . "${DIR}/../scripts/utils/utils"
 
+install_spotify() {
+  e_header "Adding Spotify"
+  e_info "Adding Spotify repository key"
+  local ADD_KEY=$(sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886 2> /dev/null)
+  [[ $ADD_KEY ]] && e_success "Spotify key added" || e_error "Error adding Spotify Key" && exit 1
+  local ADD_REPOSITORY=$(echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+ 2> /dev/null)
+ [[ $ADD_REPOSITORY ]] && e_success "Spotify Repository Added" || e_error "Error adding Spotify Repository" && exit 1
+ e_info "Updating repositories" && sudo apt-get update > /dev/null 2>&1
+ e_info "Installing Spotify"
+ INSTALL_SPOTIFY=$(sudo apt-get install spotify-client 2>/dev/null)
+ [[ $INSTALL_SPOTIFY ]] && e_success "Spotify installed" || e_error "Error installing Spotify" && exit 1
+}
+
 install_ppa() {
     local __PPA=(
       "git-core/ppa"
       "webupd8team/terminix"
       "ondrej/php"
     )
-
-    sudo -v
-    # Keep-alive: update existing sudo time stamp if set, otherwise do nothing.
-    while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
     e_header "Adding PPA"
     ADDED_PPA=0
@@ -32,4 +42,9 @@ install_ppa() {
 
 }
 
+sudo -v
+# Keep-alive: update existing sudo time stamp if set, otherwise do nothing.
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
 install_ppa
+install_spotify
