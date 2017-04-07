a303cb758e9adaa6f9d5a1b089dd6fdc71a7d955
Filipe Kiss <eu@filipekiss.com.br>
2017-04-07 12:25:31 -0300

    Adjust GPG Agent
     - Remove obsolete --write-env-file option
     - Redirect output to /dev/null to prevent useless messages

--- source/gpg-agent.zsh
+++ source/gpg-agent.zsh
@@ -2,5 +2,5 @@ if [ -f ~/.gnupg/.gpg-agent-info ] && [ -n "$(pgrep gpg-agent)" ]; then
     source ~/.gnupg/.gpg-agent-info
     export GPG_AGENT_INFO
 else
-    eval $(gpg-agent --daemon --write-env-file ~/.gnupg/.gpg-agent-info)
+    eval $(gpg-agent --daemon > /dev/null 2>&1)
 fi
