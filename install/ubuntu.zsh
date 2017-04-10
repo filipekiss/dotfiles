b9c75b744b6eaa446e19766d0a0768a12bd19887
Filipe Kiss <eu@filipekiss.com.br>
2017-04-10 15:14:00 -0300

    Update ubuntu installer script
     - Add installation function to dotfiles
     - Update dotfiles to properly install stuff

--- install/ubuntu.zsh
+++ install/ubuntu.zsh
@@ -1,50 +1,157 @@
+# Ubuntu-only stuff. Abort if not Ubuntu.
 ! has_dotfiles_function > /dev/null 2>&1 && [[ -e $HOME/.dotfiles/bin/dotfiles ]] && source $HOME/.dotfiles/bin/dotfiles "source"
 ! has_dotfiles_function > /dev/null 2>&1 && echo "Something went wrong. Try again" && exit 1
 is_ubuntu || return 1
 
+apt_keys=()
+apt_source_files=()
+apt_source_texts=()
+apt_packages=()
+deb_installed=()
+deb_sources=()
 
-trap cleanup EXIT INT
-function cleanup {
-  e_activity_end
+# Ubuntu distro release name, eg. "xenial"
+release_name=$(lsb_release -c | awk '{print $2}')
+
+function add_ppa() {
+  apt_source_texts+=($1)
+  IFS=':/' eval 'local parts=($1)'
+  apt_source_files+=("${parts[1]}-ubuntu-${parts[2]}-$release_name")
 }
 
-function update_apt() {
-  e_activity "Update apt-cache"
-  sudo apt-get update > /dev/null
-  RETVAL=$?
-  e_activity_end
-  [[ $RETVAL -eq 0 ]] && e_success "apt-cache updated" || e_error "Something went wrong updating apt-cache. Try again";
+#############################
+# WHAT DO WE NEED TO INSTALL?
+#############################
+
+# Misc.
+apt_packages+=(
+  build-essential
+  curl
+  git-core
+  htop
+  tree
+  vim
+)
+
+
+if is_ubuntu_desktop; then
+  # https://www.digitalocean.com/community/tutorials/how-to-install-and-use-docker-on-ubuntu-16-04
+  apt_keys+=('--keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D')
+  apt_source_files+=(docker)
+  apt_source_texts+=("deb https://apt.dockerproject.org/repo ubuntu-${release_name} main")
+  apt_packages+=(docker-engine)
+
+  # http://www.omgubuntu.co.uk/2016/06/install-latest-arc-gtk-theme-ubuntu-16-04
+  apt_keys+=(http://download.opensuse.org/repositories/home:Horst3180/xUbuntu_16.04/Release.key)
+  apt_source_files+=(arc-theme)
+  apt_source_texts+=("deb http://download.opensuse.org/repositories/home:/Horst3180/xUbuntu_16.04/ /")
+  apt_packages+=(arc-theme)
+
+  # https://www.ubuntuupdates.org/ppa/google_chrome
+  apt_keys+=(https://dl-ssl.google.com/linux/linux_signing_key.pub)
+  apt_source_files+=(google-chrome)
+  apt_source_texts+=("deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main")
+  apt_packages+=(google-chrome-stable)
+
+  # https://www.spotify.com/us/download/linux/
+  apt_keys+=('--keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886')
+  apt_source_files+=(spotify)
+  apt_source_texts+=("deb http://repository.spotify.com stable non-free")
+  apt_packages+=(spotify-client)
+fi
+
+function other_stuff() {
+  e_info "Adding user to docker group"
+  sudo usermod -aG docker $(whoami)
+  e_info "Installing latest version for youtube-dl"
+  sudo wget https://yt-dl.org/downloads/latest/youtube-dl -O /usr/local/bin/youtube-dl
+  sudo chmod a+rx /usr/local/bin/youtube-dl
 }
 
-function install_from_apt() {
-  local package="$1"
-  local ppa="$2"
-  HAS_PACKAGE_BINARY=$(yes_no command -v $package)
-  HAS_PACKAGE_APT=$(yes_no sudo apt-cache show $package)
-  [[ $HAS_PACKAGE_BINARY = "yes" ]] && e_success "${package} already installed" && return
-  if [[ $HAS_PACKAGE_APT = "no" ]]; then
-    if [[ -z $ppa ]]; then
-      e_error "${RESET}${package} not found and no ppa provided."
-      return 1
+####################
+# ACTUALLY DO THINGS
+####################
+
+# Add APT keys.
+keys_cache=$DOTFILES/caches/apt_keys
+IFS=$'\n' GLOBIGNORE='*' command eval 'setdiff_cur=($(<$keys_cache))'
+setdiff_new=("${apt_keys[@]}"); setdiff; apt_keys=("${setdiff_out[@]}")
+unset setdiff_new setdiff_cur setdiff_out
+
+if (( ${#apt_keys[@]} > 0 )); then
+  e_header "Adding APT keys (${#apt_keys[@]})"
+  for key in "${apt_keys[@]}"; do
+    e_arrow "$key"
+    if [[ "$key" =~ -- ]]; then
+      sudo apt-key adv $key
+    else
+      wget -qO- $key | sudo apt-key add -
+    fi && \
+    echo "$key" >> $keys_cache
+  done
+fi
+
+# Add APT sources.
+function __temp() { [[ ! -e /etc/apt/sources.list.d/$1.list ]]; }
+source_i=($(array_filter_i apt_source_files __temp))
+
+if (( ${#source_i[@]} > 0 )); then
+  e_header "Adding APT sources (${#source_i[@]})"
+  for i in "${source_i[@]}"; do
+    source_file=${apt_source_files[i]}
+    source_text=${apt_source_texts[i]}
+    if [[ "$source_text" =~ ppa: ]]; then
+      e_arrow "$source_text"
+      sudo add-apt-repository -y $source_text
+    else
+      e_arrow "$source_file"
+      sudo sh -c "echo '$source_text' > /etc/apt/sources.list.d/$source_file.list"
     fi
-    e_info "${package} not found on cache. adding ppa."
-    sudo add-apt-repository ${ppa}
-    update_apt
-  fi
-  e_activity "$package not found. Installing..."
-  sudo apt-get install -y $package > /dev/null 2>&1
-  RETVAL=$?
-  e_activity_end
-  [[ $RETVAL -eq 0 ]] && e_success "Done. ${RESET}${package} installed" || e_error "Error installing $package"
-}
-## Sudo Keep Alive by Ben Alman
-sudo -v
-## Keep-alive: update existing sudo time stamp if set, otherwise do nothing.
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+  done
+fi
 
-e_header "Install Ubuntu Stuff"
-# Update apt-cache before everything
-update_apt
+# Update APT.
+e_header "Updating APT"
+sudo apt-get -qq update
 
-## Youtube-DL
-install_from_apt "youtube-dl" "ppa:nilarimogard/webupd8"
+# Only do a dist-upgrade on initial install, otherwise do an upgrade.
+if is_dotfiles_bin; then
+  sudo apt-get -qq upgrade
+else
+  sudo apt-get -qq dist-upgrade
+fi
+
+# Install APT packages.
+installed_apt_packages="$(dpkg --get-selections | grep -v deinstall | awk 'BEGIN{FS="[\t:]"}{print $1}' | uniq)"
+apt_packages=($(setdiff "${apt_packages[*]}" "$installed_apt_packages"))
+
+if (( ${#apt_packages[@]} > 0 )); then
+  e_header "Installing APT packages (${#apt_packages[@]})"
+  for package in "${apt_packages[@]}"; do
+    e_arrow "$package"
+    [[ "$(type -t preinstall_$package)" == function ]] && preinstall_$package
+    sudo apt-get -qq install "$package" && \
+    [[ "$(type -t postinstall_$package)" == function ]] && postinstall_$package
+  done
+fi
+
+# Install debs via dpkg
+function __temp() { [[ ! -e "$1" ]]; }
+deb_installed_i=($(array_filter_i deb_installed __temp))
+
+if (( ${#deb_installed_i[@]} > 0 )); then
+  installers_path="$DOTFILES/caches/installers"
+  mkdir -p "$installers_path"
+  e_header "Installing debs (${#deb_installed_i[@]})"
+  for i in "${deb_installed_i[@]}"; do
+    e_arrow "${deb_installed[i]}"
+    deb="${deb_sources[i]}"
+    [[ "$(type -t "$deb")" == function ]] && deb="$($deb)"
+    installer_file="$installers_path/$(echo "$deb" | sed 's#.*/##')"
+    wget -O "$installer_file" "$deb"
+    sudo dpkg -i "$installer_file"
+  done
+fi
+
+# Run anything else that may need to be run.
+type -t other_stuff >/dev/null && other_stuff
