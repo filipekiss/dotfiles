1c51466d1ba2316296e442f05aec295da0706ecc
Filipe Kiss <eu@filipekiss.com.br>
2017-04-11 15:18:26 -0300

    Add o/open alias
     - Make sure we use xdg-open if available

---
+++ source/functions/o.zsh
@@ -0,0 +1,20 @@
+# No arguments: `open .`
+# With arguments: acts like `open`
+
+unalias o 2> /dev/null
+
+if (( $+commands[xdg-open] )); then
+  #if it's linux, we use xdg-open instead of open
+  alias open="xdg-open &> /dev/null"
+fi
+
+function o {
+  if [[ $# -eq 0 ]]; then
+    open .
+  else
+    open "$@"
+  fi
+}
+
+# Complete o like open
+compdef o=open
