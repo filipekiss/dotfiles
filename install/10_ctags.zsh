379f016a42dde854ef12cf0ab6e79234b4bc30ab
Filipe Kiss <eu@filipekiss.com.br>
2017-04-12 18:08:58 -0300

    Add ctags installer

---
+++ install/10_ctags.zsh
@@ -0,0 +1,20 @@
+# Ubuntu-only stuff. Abort if not Ubuntu.
+! has_dotfiles_function > /dev/null 2>&1 && [[ -e $HOME/.dotfiles/bin/dotfiles ]] && source $HOME/.dotfiles/bin/dotfiles "source"
+! has_dotfiles_function > /dev/null 2>&1 && echo "Something went wrong. Try again" && exit 1
+is_ubuntu || return 1
+
+[[ $+commands[ctags] ]] || return 0
+
+CTAGS_REPO="https://github.com/universal-ctags/ctags.git"
+
+e_info "Cloning Universal Ctags"
+cd /tmp
+e_info $PWD
+git clone $CTAGS_REPO ctags
+cd ctags
+e_info "Configuring"
+./autogen.sh
+./configure
+sudo make
+e_info "Installing"
+sudo make install
