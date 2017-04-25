ed644cd4a3d88c95f1eec8e6a171a68f6de5d4b5
Filipe Kiss <eu@filipekiss.com.br>
2017-04-25 08:04:00 -0300

    Add proper transfer function
     - Uses transfer.sh

---
+++ source/functions/transfer.zsh
@@ -0,0 +1,54 @@
+# See http://transfer.sh/ and https://gist.github.com/nl5887/a511f172d3fb3cd0e42d#file-transfer-sh
+#
+# USAGE: transfer <file>
+
+if (( ! $+commands[curl] )); then
+  return 1
+fi
+
+transfer() {
+    # check arguments
+    if [ $# -eq 0 ];
+    then
+          e_error "${RESET}No arguments specified. Usage:\n transfer test.md"
+        return 1
+    fi
+
+    # get temporarily filename, output is written to this file show progress can be showed
+    tmpfile=$( mktemp -t transferXXX )
+
+    # upload stdin or file
+    file=$1
+
+    if tty -s;
+    then
+        basefile=$(basename "$file" | sed -e 's/[^a-zA-Z0-9._-]/-/g')
+
+        if [ ! -e $file ];
+        then
+            e_error "${RESET}File $file doesn't exists."
+            return 1
+        fi
+
+        if [ -d $file ];
+        then
+            # zip directory and transfer
+            zipfile=$( mktemp -t transferXXX.zip )
+            cd $(dirname $file) && zip -r -q - $(basename $file) >> $zipfile
+            curl --progress-bar --upload-file "$zipfile" "https://transfer.sh/$basefile.zip" >> $tmpfile
+            rm -f $zipfile
+        else
+            # transfer file
+            curl --progress-bar --upload-file "$file" "https://transfer.sh/$basefile" >> $tmpfile
+        fi
+    else
+        # transfer pipe
+        curl --progress-bar --upload-file "-" "https://transfer.sh/$file" >> $tmpfile
+    fi
+
+    # cat output link
+    cat $tmpfile
+
+    # cleanup
+    rm -f $tmpfile
+}
