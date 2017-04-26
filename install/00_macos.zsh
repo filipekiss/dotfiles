09f16616805ede52b27fd18cd767d5a9b381fc8e
Filipe Kiss <eu@filipekiss.com.br>
2017-04-26 15:22:47 -0300

    Add homebrew installation files

---
+++ install/00_macos.zsh
@@ -0,0 +1,25 @@
+# macOS-only stuff. Abort if not macOS.
+(( ! $+functions[has_dotfiles_function] )) && [[ -e $HOME/.dotfiles/bin/dotfiles ]] && source $HOME/.dotfiles/bin/dotfiles "source"
+(( ! $+functions[has_dotfiles_function] )) && echo "Something went wrong. Try again" && exit 1
+is_macos || return 1
+
+function validate_homebrew() {
+  if (( ! $+commands[brew] )) && is_macos; then
+    e_info "Installing Homebrew"
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  fi
+}
+
+function install_from_brewfile() {
+  BREWFILE="${DOTFILES}/homebrew/Brewfile"
+  e_info "Updating Homebrew"
+  brew update
+  e_info "Installing brewfile $BREWFILE"
+  brew bundle --file=$BREWFILE
+  e_info "Post-Install Clean-up"
+  brew cleanup
+  brew doctor
+}
+
+validate_homebrew
+install_from_brewfile
