03591a2cef54586bbe4097b5dafdc96b686004cf
Filipe Kiss <eu@filipekiss.com.br>
2017-04-26 17:58:37 -0300

    Update how atom packages are installed
     - Prevent trying to update all packages we have. APM doesn't do this yet, so we do it our way

--- post-install/00_macos.zsh
+++ post-install/00_macos.zsh
@@ -5,7 +5,11 @@ is_macos || return 1
 
 function install_atom_packages() {
   e_info "Installing packages from ${ATOM_PACKAGES_FILE}"
-  $APM_BIN install --packages-file $ATOM_PACKAGES_FILE
+  # We use this method to just install new packages. NO updates. apm install doesn't handle this well yet
+  for package ($(cat $ATOM_PACKAGES_FILE )); do
+    [[ -e $HOME/.atom/packages/${package%%@*} ]] && e_success "${RESET}${package%%@*} already installed" && continue
+    $APM_BIN install ${package}
+  done;
 }
 
 function update_ssh_permissions() {
