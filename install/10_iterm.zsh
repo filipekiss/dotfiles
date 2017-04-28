4e0916ba09c4883fa06e1952cfbef39267d48a93
Filipe Kiss <eu@filipekiss.com.br>
2017-04-28 12:29:55 -0300

    Update Nord Colors installer
     - Use git repo instead of curling our way around stuff

--- install/10_iterm.zsh
+++ install/10_iterm.zsh
@@ -3,34 +3,28 @@
 (( ! $+functions[has_dotfiles_function] )) && echo "Something went wrong. Try again" && exit 1
 is_macos || return 1
 
-NORD_ITERM_RELEASES="https://github.com/arcticicestudio/nord-iterm2/releases/latest"
-GITHUB_RELEASES_PREFIX="https://github.com"
-TEMP_FILE_NAME="/tmp/nord-iterm-latest.tar.gz"
+NORD_ITERM_REPO_URL=https://github.com/arcticicestudio/nord-iterm2.git
+NORD_LOCAL_REPO=$DOTFILES/caches/downloads/nord-iterm2
 
-function get_latest_release() {
-  local version_url
-  version_url=$(curl -sL $NORD_ITERM_RELEASES | grep "/archive/.*\.tar.gz" | cut -d '"' -f2)
-  echo "${GITHUB_RELEASES_PREFIX}${version_url}"
-}
-
-function download_nord_theme() {
-  local final_url
-  if [[ ! -f "$TEMP_FILE_NAME" ]]; then
-    e_info "Retrieving URL"
-    final_url=$(get_latest_release)
-    e_info "Downloading Nord Theme for iTerm"
-    sleep 0.5
-    curl -o $TEMP_FILE_NAME -l $final_url
-  else
-    e_info "Cached file found at $TEMP_FILE_NAME"
-  fi
-  e_info "Extracting Nord Theme"
-  cd /tmp
-  extract nord-iterm-latest.tar.gz
-  cd nord-iterm2*
+function update_iterm_config() {
+  cd $NORD_LOCAL_REPO
   e_info "Adding Color Scheme to iTerm"
-  open -a iTerm /tmp/nord-iterm2*/src/xml/*.itermcolors
+  open -a iTerm "${NORD_LOCAL_REPO}/src/xml/Nord.itermcolors"
   e_success "${RESET}Done"
 }
-
-download_nord_theme
+if [[ ! -d $NORD_LOCAL_REPO ]]; then
+  e_info "Nord Colors for iTerm2 not found. Cloning..."
+  git clone $NORD_ITERM_REPO_URL $NORD_LOCAL_REPO
+  update_iterm_config
+else
+  cd $NORD_LOCAL_REPO
+  prev_head="$(git rev-parse HEAD)"
+  git reset --hard HEAD
+  git pull
+  current_head="$(git rev-parse HEAD)"
+  if [[ $current_head != $prev_head ]]; then
+    update_iterm_config
+  else
+    e_info "Already at latest version"
+  fi
+fi
