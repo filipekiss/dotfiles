59ac63f6e81994fc2518581e4662e6370dc2d653
Filipe Kiss <eu@filipekiss.com.br>
2017-04-28 09:29:51 -0300

    Add cask command
     - Alias to `brew cask`

---
+++ source/functions/cask.zsh
@@ -0,0 +1,20 @@
+# Alias for `brew cask`
+#
+# No arguments will work as `brew cask list`. Otherwise will behave as `brew cask`
+
+is_macos || return 0
+
+[[ $+commands[brew] ]] || return 0
+
+unalias cask 2> /dev/null
+
+function cask() {
+  if [[ $# -eq 0 ]]; then
+    brew cask list
+  else
+    brew cask $@
+  fi
+}
+
+# Autocomplete as if typing `brew cask`
+compdef '_brew_cask' cask
