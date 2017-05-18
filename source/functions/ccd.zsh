eb89abd84b88803dcd2262269419c18b54c179bd
Filipe Kiss <eu@filipekiss.com.br>
2017-05-18 18:49:07 -0300

    Add ccd functions
     - Cd to Bookmarked directories using FZF

---
+++ source/functions/ccd.zsh
@@ -0,0 +1,17 @@
+# Setup ccd function
+# ------------------
+unalias ccd 2> /dev/null
+ccd() {
+   local dest_dir=$(cdscuts_glob_echo | fzf )
+   if [[ $dest_dir != '' ]]; then
+     dest_dir=${dest_dir:s/~/$HOME}
+      cd "${dest_dir%% *}"
+   fi
+}
+
+ccd-add () {
+  [[ ! -f ~/.ccdpaths ]] && touch ~/.ccdpaths
+  if ! grep -Fq "${PWD}" ~/.ccdpaths; then
+    echo "${PWD} # $*" >>! ~/.ccdpaths
+  fi
+}
