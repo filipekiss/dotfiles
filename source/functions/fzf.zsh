a8711b1a82e78ae158de9fb6bba7377f243c869d
Filipe Kiss <eu@filipekiss.com.br>
2017-05-18 10:27:19 -0300

    Add a few FZF Functions

--- source/functions/fzf.zsh
+++ source/functions/fzf.zsh
@@ -45,16 +45,24 @@ fco() {
   git checkout $(echo "$target" | awk '{print $2}')
 }
 # fbr - checkout git branch
+# fbr() {
+#   local branches target
+#   branches=$(
+#     git branch --all | grep -v HEAD             |
+#     sed "s/.* //"    | sed "s#remotes/[^/]*/##" |
+#     sort -u          | awk '{print "\x1b[34;1mbranch\x1b[m\t" $1}') || return
+#   target=$(
+#     (echo "$branches") | sed '/^$/d' |
+#     fzf-down --no-hscroll --reverse --ansi +m -d "\t" -n 2 -q "$*") || return
+#   git checkout $(echo "$target" | awk '{print $2}')
+# }
+
 fbr() {
-  local branches target
-  branches=$(
-    git branch --all | grep -v HEAD             |
-    sed "s/.* //"    | sed "s#remotes/[^/]*/##" |
-    sort -u          | awk '{print "\x1b[34;1mbranch\x1b[m\t" $1}') || return
-  target=$(
-    (echo "$branches") | sed '/^$/d' |
-    fzf-down --no-hscroll --reverse --ansi +m -d "\t" -n 2 -q "$*") || return
-  git checkout $(echo "$target" | awk '{print $2}')
+  local branches branch
+  branches=$(git for-each-ref --count=30 --sort=-committerdate refs/heads/ --format="%(refname:short)") &&
+  branch=$(echo "$branches" |
+           fzf-tmux -d $(( 2 + $(wc -l <<< "$branches") )) +m) &&
+  git checkout $(echo "$branch" | sed "s/.* //" | sed "s#remotes/[^/]*/##")
 }
 # ftag - checkout git tag
 ftag() {
@@ -64,4 +72,32 @@ ftag() {
     (echo "$tags") | sed '/^$/d' |
     fzf-down --no-hscroll --reverse --ansi +m -d "\t" -n 2 -q "$*") || return
   git checkout $(echo "$target" | awk '{print $2}')
+}
+# fstash - easier way to deal with stashes
+# type fstash to get a list of your stashes
+# enter shows you the contents of the stash
+# ctrl-d shows a diff of the stash against your current HEAD
+# ctrl-b checks the stash out as a branch, for easier merging
+fstash() {
+  local out q k sha
+  while out=$(
+    git stash list --pretty="%C(yellow)%h %>(14)%Cgreen%cr %C(blue)%gs" |
+    fzf --ansi --no-sort --query="$q" --print-query \
+        --expect=ctrl-d,ctrl-b);
+  do
+    mapfile -t out <<< "$out"
+    q="${out[0]}"
+    k="${out[1]}"
+    sha="${out[-1]}"
+    sha="${sha%% *}"
+    [[ -z "$sha" ]] && continue
+    if [[ "$k" == 'ctrl-d' ]]; then
+      git diff $sha
+    elif [[ "$k" == 'ctrl-b' ]]; then
+      git stash branch "stash-$sha" $sha
+      break;
+    else
+      git stash show -p $sha
+    fi
+  done
 }
