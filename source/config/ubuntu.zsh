aed312bcc8e316db9cbc92586c58de19a6c10b04
Filipe Kiss <eu@filipekiss.com.br>
2017-06-19 11:17:15 -0300

    meta:  Remove linux specific settings
     - Maybe create a different folder for os-specific stuff?
    For now, removes linux (see ubuntu branch if you want these files).
    In the future, we may have a macos and a linux specific repo

--- source/config/ubuntu.zsh
+++
@@ -1,7 +0,0 @@
-is_ubuntu || return 1
-
-# Package management
-alias update="sudo apt-get -qq update && sudo apt-get upgrade"
-alias install="sudo apt-get install"
-alias remove="sudo apt-get remove"
-alias search="apt-cache search"
--- source/functions/pbcopypaste.zsh
+++
@@ -1,6 +0,0 @@
-is_ubuntu || return 0
-
-[[ $+commands[xclip] ]] || return 0
-
-alias pbcopy='xclip -selection clipboard'
-alias pbpaste='xclip -selection clipboard -o'
