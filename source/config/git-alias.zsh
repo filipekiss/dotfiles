0c8e9287f11209313856d5948c8e41da46b1e686
Filipe Kiss <eu@filipekiss.com.br>
2017-07-06 10:22:38 -0300

    zsh:  Add new git aliases

---
+++ source/config/git-alias.zsh
@@ -0,0 +1,7 @@
+# Even shorter git aliases
+alias gci="git commit"
+alias gco="git checkout"
+alias gps="git push"
+alias gpr="git pull --rebase"
+alias gst="git status --short"
+alias gap="git add -p"
