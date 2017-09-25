aee156e387769a164e632cc3dcdaaf0405b919d1
Filipe Kiss <eu@filipekiss.com.br>
2017-09-25 11:49:35 -0300

    utils:  Update iterm script
     - Allow multiple repositories

--- scripts/iterm.zsh
+++ scripts/iterm.zsh
@@ -3,28 +3,42 @@
 (( ! $+functions[has_dotfiles_function] )) && echo "Something went wrong. Try again" && exit 1
 is_macos || return 1
 
-NORD_ITERM_REPO_URL=https://github.com/arcticicestudio/nord-iterm2.git
-NORD_LOCAL_REPO=$DOTFILES/caches/downloads/nord-iterm2
+DOWNLOAD_LOCATION=$DOTFILES/caches/downloads/
+
+ITERM_THEMES_REPOS=(
+    https://github.com/arcticicestudio/nord-iterm2.git
+    https://github.com/herrbischoff/iterm2-gruvbox
+)
 
 function update_iterm_config() {
-  cd $NORD_LOCAL_REPO
-  e_info "Adding Color Scheme to iTerm"
-  open -a iTerm "${NORD_LOCAL_REPO}/src/xml/Nord.itermcolors"
+  local package_location="$1"
+  cd $package_location
+  e_info "Adding ${package_location:t:r} to iTerm"
+  local itermfiles=$(find $package_location -type f -iname "*.itermcolors")
+  for file ($itermfiles) open -a iTerm "$file"
   e_success "${RESET}Done"
 }
-if [[ ! -d $NORD_LOCAL_REPO ]]; then
-  e_info "Nord Colors for iTerm2 not found. Cloning..."
-  git clone $NORD_ITERM_REPO_URL $NORD_LOCAL_REPO
-  update_iterm_config
-else
-  cd $NORD_LOCAL_REPO
-  prev_head="$(git rev-parse HEAD)"
-  git reset --hard HEAD
-  git pull
-  current_head="$(git rev-parse HEAD)"
-  if [[ $current_head != $prev_head ]]; then
-    update_iterm_config
-  else
-    e_info "Already at latest version"
-  fi
-fi
+
+function download_or_update_package() {
+    local repo_url="$1"
+    local package_name=${repo_url:t:r}
+    local package_location="${DOWNLOAD_LOCATION}/${package_name}"
+    if [[ ! -d $package_location ]]; then
+        e_info "${package_name} for iTerm2 not found. Cloning..."
+        git clone $repo_url $package_location
+        update_iterm_config "${package_location}"
+    else
+        cd ${package_location}
+        prev_head="$(git rev-parse HEAD)"
+        git reset --hard HEAD
+        git pull
+        current_head="$(git rev-parse HEAD)"
+        if [[ $current_head != $prev_head ]]; then
+            update_iterm_config ${pacakge_location}
+        else
+            e_info "${package} Already at latest version"
+        fi
+    fi
+}
+
+for PACKAGE ($ITERM_THEMES_REPOS) download_or_update_package "$PACKAGE"
