b80ce507c701e597aa5e4f65827b9f579f5990d6
Filipe Kiss <eu@filipekiss.com.br>
2017-09-26 15:41:10 -0300

    utils:  Update afk function
    Apple changed, once again, how the `ScreenSaverEngine` is invoked via
    CLI on macOS 10.13 (High Sierra). I've updated the function to check if
    the `ScreenSaverEngine.app` is on the Frameworks folder. It it's there,
    open it using the full path. Otherwise, rely on `/usr/bin/open` to find
    the proper app and run it for us. I hope this is the last time I need to
    change this.

--- source/functions/afk.zsh
+++ source/functions/afk.zsh
@@ -4,5 +4,13 @@ is_macos || return 0
 
 
 function afk() {
-  /usr/bin/open /System/Library/Frameworks/ScreenSaver.framework/Versions/A/Resources/ScreenSaverEngine.app
+  local sierra_screensaver_location="/System/Library/Frameworks/ScreenSaver.framework/Versions/A/Resources/ScreenSaverEngine.app"
+  local app_name="ScreenSaverEngine.app"
+  # On macOS Sierra, the screensaver has to be acessed directly. On other versions, just try to open
+  # the app by name
+  if [[ -f $sierra_screensaver_location ]]; then
+    /usr/bin/open /System/Library/Frameworks/ScreenSaver.framework/Versions/A/Resources/ScreenSaverEngine.app
+  else
+    /usr/bin/open -a $app_name
+  fi
 }
