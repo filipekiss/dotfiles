8f0e035af60292fbb207ff99ac19dc722be37fd1
Filipe Kiss <eu@filipekiss.com.br>
2017-10-17 18:00:35 -0200

    meta: Update permissions script

--- scripts/file-permissions.zsh
+++ scripts/file-permissions.zsh
@@ -3,19 +3,18 @@
 
 function update_ssh_permissions() {
   e_info "Setting ~/.ssh folders to 0700"
-  find $HOME/.ssh -exec chmod 0700 {} \;
+  find $HOME/.ssh -exec chmod -v 0700 {} \;
   e_info "Setting ~/.ssh files to 0600"
-  find $HOME/.ssh/* -type f -exec chmod 0600 {} \;
-  find $HOME/.ssh/known_hosts -type f -exec chmod 0744 {} \;
+  find $HOME/.ssh/* -type lf -exec chmod -v 0600 {} \;
+  find $HOME/.ssh/known_hosts -type lf -exec chmod -v 0744 {} \;
   e_success "${RESET} .ssh correctly set"
 }
 
 function update_gnupg_permissions() {
   e_info "Setting ~/.gnupg folders to 0700"
-  find $HOME/.gnupg -exec chmod 0700 {} \;
+  find $HOME/.gnupg -exec chmod -v 0700 {} \;
   e_info "Setting ~/.gnupg files to 0600"
-  find $HOME/.gnupg/* -type f -exec chmod 0600 {} \;
-  find $HOME/.gnupg/* -type s -exec chmod 0600 {} \;
+  find $HOME/.gnupg/* -type lf -exec chmod -v 0600 {} \;
   e_success "${RESET} .gnupg correctly set"
 }
 
