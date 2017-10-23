4e89384115fbb351eaee1482c50e41bd84ee6e75
Filipe Kiss <eu@filipekiss.com.br>
2017-10-23 17:12:17 -0200

    utils: Add pomo function
    The `pomo` function will invoke `bin/pomodoro` and, if on macOS and the
    Focus app is installed will also trigger the Focus app. If you exit the
    `pomodoro` running on your CLI it should also stop the Focus app.

---
+++ source/functions/pomodoro.zsh
@@ -0,0 +1,14 @@
+HAS_POMODORO_BIN=$commands[pomodoro]
+CACHED_VARS_FILE=$HOME/.dot_var_cache
+[[ -f $CACHED_VARS_FILE ]] && source $CACHED_VARS_FILE
+[[ -z $HAS_FOCUS_APP ]] && HAS_FOCUS_APP=$(find /Applications/Setapp /Applications -maxdepth 1 -iname Focus.app -print -quit -maxdepth 1 2>/dev/null) && echo "export HAS_FOCUS_APP=\"$HAS_FOCUS_APP\""| tee -a $CACHED_VARS_FILE > /dev/null
+
+[[ -z $HAS_FOCUS_APP && -z $HAS_POMODORO_BIN ]] && exit 0
+
+unalias pomo 2>/dev/null
+
+function pomo() {
+    POMO_TIMER='25m'
+    [[ $HAS_FOCUS_APP ]] && open 'focus://focus?minutes=25' && POMO_TIMER='25m2s'
+    [[ $HAS_POMODORO_BIN ]] && pomodoro $POMO_TIMER; open 'focus://unfocus'
+}
