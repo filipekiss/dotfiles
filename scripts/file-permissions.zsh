f732db7e9423a5f6e54f146c22d822d7643a42cb
Filipe Kiss <eu@filipekiss.com.br>
2017-11-16 18:25:43 -0200

    meta:  Make permissions script more portable

--- scripts/file-permissions.zsh
+++ scripts/file-permissions.zsh
@@ -5,8 +5,8 @@ function update_ssh_permissions() {
   e_info "Setting ~/.ssh folders to 0700"
   find $HOME/.ssh -exec chmod -v 0700 {} \;
   e_info "Setting ~/.ssh files to 0600"
-  find $HOME/.ssh/* -type lf -exec chmod -v 0600 {} \;
-  find $HOME/.ssh/known_hosts -type lf -exec chmod -v 0744 {} \;
+  find $HOME/.ssh/* \( -type l -o -type f \) -exec chmod -v 0600 {} \;
+  find $HOME/.ssh/known_hosts \( -type l -o -type f \) -exec chmod -v 0744 {} \;
   e_success "${RESET} .ssh correctly set"
 }
 
@@ -14,7 +14,7 @@ function update_gnupg_permissions() {
   e_info "Setting ~/.gnupg folders to 0700"
   find $HOME/.gnupg -exec chmod -v 0700 {} \;
   e_info "Setting ~/.gnupg files to 0600"
-  find $HOME/.gnupg/* -type lf -exec chmod -v 0600 {} \;
+  find $HOME/.gnupg/* \( -type l -o -type f \) -exec chmod -v 0600 {} \;
   e_success "${RESET} .gnupg correctly set"
 }
 
