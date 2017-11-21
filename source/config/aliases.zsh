f23fd2c858746c59fc30b5728a9ec49fda5ea9b8
Filipe Kiss <eu@filipekiss.com.br>
2017-11-21 15:59:17 -0200

    :bug: git: Update aliases
     - Add `work` alias to call `mx saraiva`
     - Fix `gpr` alias to call `git-up` binary directly. This prevents a few
     "Cannot rebase onto multiple branches" error I was getting

--- source/config/aliases.zsh
+++ source/config/aliases.zsh
@@ -14,6 +14,7 @@ alias server="sudo python -m SimpleHTTPServer 80"
 alias type="${saliases[type]:-type} -a"
 alias gulp="nocorrect gulp"
 alias weechat="weechat --dir=$HOME/.dotfiles/irc/.weechat"
+alias work="mx saraiva"
 
 # File Download
 if (( $+commands[curl] )); then
