4442da4d37c1556350c7bb2db6b54f03d866734a
Filipe Kiss <eu@filipekiss.com.br>
2017-12-07 17:40:54 -0200

    utils: Set screensaver lock to 30s on macos

--- scripts/macos-options.zsh
+++ scripts/macos-options.zsh
@@ -225,7 +225,7 @@ e_header "Screen"
 
 e_info 'Require password immediately after sleep or screen saver begins'
 defaults write com.apple.screensaver askForPassword -int 1
-defaults write com.apple.screensaver askForPasswordDelay -int 0
+defaults write com.apple.screensaver askForPasswordDelay -int 30
 
 e_info 'Save screenshots to the desktop'
 defaults write com.apple.screencapture location -string "${HOME}/Desktop"
