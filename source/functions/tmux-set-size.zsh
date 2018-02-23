b9ba3864c4eb89aabbfdd98ef163e854027e3221
Filipe Kiss <eu@filipekiss.com.br>
2018-02-23 14:52:22 -0300

    :bug: tmux: Ensure tmux-set-size actually sets the size

--- source/functions/tmux-set-size.zsh
+++ source/functions/tmux-set-size.zsh
@@ -8,16 +8,18 @@ function tmux-set-size() {
             let "finalSize=$currentWidth - $size"
         else
             let "finalSize=$size - $currentWidth"
+            [[ $direction == '-R' ]] && direction="-L" || direction="-R"
         fi
     else
         if [[ $currentHeight -gt $size ]]; then
             let "finalSize=$currentHeight - $size"
         else
             let "finalSize=$size - $currentHeight"
+            [[ $direction == '-U' ]] && direction="-D" || direction="-R"
         fi
     fi
     if [[ $finalSize -eq 0 ]]; then
-        echo "Hey! The pane is already at ${size}"
+        echo "→ The pane is already at ${size}"
         return
     fi
     tmux resize-pane ${direction} ${finalSize}
