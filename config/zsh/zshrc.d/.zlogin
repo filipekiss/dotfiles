d16b0eae8050e25bf895d77e6f003babc93dd16f
Filipe Kiss <eu@filipekiss.com.br>
2018-03-07 09:28:28 -0300

    zsh: Remove zim framework
    Less bloat loaded, trying to make the session startup time faster!

--- config/zsh/zshrc.d/.zlogin
+++ config/zsh/zshrc.d/.zlogin
@@ -1,6 +1,3 @@
 #
 # User configuration sourced by login shells
 #
-
-# Initialize zim
-[[ -s ${ZIM_HOME}/login_init.zsh ]] && source ${ZIM_HOME}/login_init.zsh
--- source/config/highlight.zsh
+++ source/config/highlight.zsh
@@ -2,6 +2,8 @@
 # The syntax for values is the same as the syntax of "types of highlighting" of
 # the zsh builtin $zle_highlight array, which is documented in the zshzle(1)
 # manual page. http://zsh.sourceforge.net/Doc/Release/Zsh-Line-Editor.html#Character-Highlighting
+#
+# +TODO: Find out how to make custom highlighters work with zdharma/fast-highlight
 
 # Commands that start with `rm -rf` will be printed in red
 ZSH_HIGHLIGHT_PATTERNS+=('rm -rf *' 'fg=white,bold,bg=red')
