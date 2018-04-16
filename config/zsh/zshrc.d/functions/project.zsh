c6dcce8d4a56bf07466a89ffccc9f386bbc13626
Filipe Kiss <eu@filipekiss.com.br>
2018-04-16 10:44:35 -0300

    zsh: Update project function
     - Add `clone` command
     - Better completion function

--- config/zsh/zshrc.d/functions/project.zsh
+++ config/zsh/zshrc.d/functions/project.zsh
@@ -1,4 +1,3 @@
-
 unalias project 2>/dev/null
 
 
@@ -9,8 +8,11 @@ function project() {
         create)
             __create_project $@
             ;;
+        clone)
+            __clone_project $@
+            ;;
         *)
-            echo "Command Invalid. Try project create <namespace> <project>"
+            echo "Command Invalid."
             ;;
     esac
 }
@@ -19,6 +21,7 @@ function project() {
 function __create_project() {
     local projectNamespace="$1"
     local projectName="${2}"
+    [[ -z $projectName ]] && echo "You need a project name!\nTry project create ${projectNamespace} my-cool-project" && return 1
     local projectLocation="${PROJECTS:-${HOME}}/${projectNamespace}/${projectName}"
     if [[ -d ${projectLocation} ]]; then
         e_info "Project ${projectNamespace}/${projectName} already exists. Wish to go there? [Y/n]"
@@ -29,6 +32,25 @@ function __create_project() {
     else
         e_info "Creating $projectLocation"
         mkdir -p "${projectLocation}"
+    fi
+    cd ${projectLocation}
+}
+
+function __clone_project() {
+    local projectNamespace="$1"
+    local projectName="${2}"
+    [[ -z $projectName ]] && echo "You need a project name!\nTry project clone ${projectNamespace} my-cool-project" && return 1
+    local projectLocation="${PROJECTS:-${HOME}}/${projectNamespace}/${projectName}"
+    if [[ -d ${projectLocation} ]]; then
+        e_info "Project ${projectNamespace}/${projectName} already exists. Wish to go there? [Y/n]"
+        read -sk 1 "goToProject"
+        if [[ ${goToProject:-y} != ^[Yy]$ ]]; then
+            return 0
+        fi
+    else
+        e_info "Cloning $projectLocation"
+        command git clone "https://github.com/${projectNamespace}/${projectName}" "${PROJECTS:-${HOME}}/${projectNamespace}/${projectName}" && e_info "Project cloned into ${PROJECTS}/${projectNamespace}/${projectName}"
+        [[ $status -gt 0 ]] && e_error "And error ocurred when cloning ${projectNamespace}/${projectName}" && return 127
     fi
     cd ${projectLocation}
 }
