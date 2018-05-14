048aa43b63a1f72412ffcbe51f85b99521b62e57
Filipe Kiss <eu@filipekiss.com.br>
2018-05-13 22:11:42 -0300

    zsh: Things that were in zprofile are mostly now in zsh/rc.d/config/environment.zsh

--- config/zsh/rc.d/config/environment.zsh
+++ config/zsh/rc.d/config/environment.zsh
@@ -1,3 +1,4 @@
+# @TODO: Organize this file, it's a mess
 #
 # generic options and environment settings
 #
@@ -44,3 +45,81 @@ if (($+ztermtitle)); then
       ;;
   esac
 fi
+
+#
+# Less
+#
+
+# Set the default Less options.
+# Mouse-wheel scrolling has been disabled by -X (disable screen clearing).
+# Remove -X and -F (exit if the content fits on one screen) to enable it.
+export LESS='-F -g -i -M -R -S -w -X -z-4'
+
+# Set the Less input preprocessor.
+# Try both `lesspipe` and `lesspipe.sh` as either might exist on a system.
+if (( $#commands[(i)lesspipe(|.sh)] )); then
+    export LESSOPEN="| /usr/bin/env $commands[(i)lesspipe(|.sh)] %s 2>&-"
+    (( $+commands[pygmentize] )) && export LESSCOLORIZER=pygmentize
+fi
+
+#
+# Temporary Files
+#
+
+if [[ ! -d "$TMPDIR" ]]; then
+    export TMPDIR="/tmp/$LOGNAME"
+    mkdir -p -m 700 "$TMPDIR"
+fi
+
+TMPPREFIX="${TMPDIR%/}/zsh"
+
+#
+# Custom env variables
+#
+
+export PROJECTS="${HOME}/code"
+[[ ! -d "$PROJECTS" ]] && mkdir -p ${PROJECTS}
+
+[[ -f ~/.zsh_base16_theme ]] && . ~/.zsh_base16_theme
+
+# Executes commands at login pre-zshrc.
+
+##############################################################
+# Set Interactive shell options
+##############################################################
+
+setopt noflowcontrol   # disable start (C-s) and stop (C-q) characters
+setopt interactivecomments  # allow comments, even in interactive shells
+setopt printexitvalue       # for non-zero exit status
+setopt HIST_EXPIRE_DUPS_FIRST    # Expire a duplicate event first when trimming history.
+setopt CORRECT
+
+# Better spell checking & auto correction prompt
+export SPROMPT="zsh: correct %F{red}'%R'%f to %F{blue}'%r'%f [%B%Uy%u%bes, %B%Un%u%bo, %B%Ue%u%bdit, %B%Ua%u%bbort]? "
+
+#
+# Editors
+# Set nvim as editor or use vim if nvim is not available
+(( $+commands[nvim] )) && export EDITOR=$commands[nvim] || export EDITOR=vim
+export VISUAL=$EDITOR
+export GIT_EDITOR=$EDITOR
+# Set less or more as the default pager.
+if (( ${+commands[emojify]} )); then
+    export PAGER="emojify | less"
+elif (( ${+commands[less]} )); then
+    export PAGER=less
+else
+    export PAGER=more
+fi
+(( $+commands[nvim] )) && export MANPAGER="nvim -c 'set ft=man' -"
+
+
+# Git options
+export GIT_MERGE_AUTOEDIT=no
+
+#
+# Language
+#
+
+export LANG='en_US.UTF-8'
+export LC_ALL='en_US.UTF-8'
