60c17ceb1e0e53c1229925ccb40d1e0f3875477c
Filipe Kiss <eu@filipekiss.com.br>
2018-05-22 12:05:19 -0300

    misc: Update prettier config

--- config/misc/prettier.config.js
+++ config/misc/prettier.config.js
@@ -1,10 +1,11 @@
 module.exports = {
-	useTabs: true, // Use spaces and not tabs
-	tabWidth: 4, // Ident using 4 spaces
-	semi: true, // Append semicolons to end of statement,
-	singleQuote: true, // Use single quote for strings,
-	trailingComma: 'es5', // Add trailing comma when valid. See https://prettier.io/docs/en/options.html#trailing-commas
-	bracketSpacing: false, // Change {foo: bar} to { foo: bar }
-	jsxBracketSameLine: false, // See https://prettier.io/docs/en/options.html#jsx-brackets
-	arrowParens: 'avoid', // Don't wrap arros functions in parens if not needed (example => { example.toUpperCase() } instead of (example) => { return example.toUpperCase() }
+    useTabs: false, // Use spaces and not tabs
+    tabWidth: 4, // Ident using 4 spaces
+    semi: true, // Append semicolons to end of statement,
+    singleQuote: true, // Use single quote for strings,
+    trailingComma: 'es5', // Add trailing comma when valid. See https://prettier.io/docs/en/options.html#trailing-commas
+    bracketSpacing: false, // Change { foo: bar } to {foo: bar}
+    jsxBracketSameLine: false, // See https://prettier.io/docs/en/options.html#jsx-brackets
+    arrowParens: 'always', // For consistency. See https://prettier.io/docs/en/options.html#arrow-function-parentheses
+    proseWrap: 'always'
 };
