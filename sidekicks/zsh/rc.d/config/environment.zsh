9381e4e9943c46aee024f8d5be10e78ab1916831
Filipe Kiss <hello@filipekiss.com.br>
2018-08-14 09:47:45 -0300

    :bug: Export EDITOR with name only
    Since I use the $EDITOR variable to set the MANPAGER and other stuff,
    ensure the variable is set with the EDITOR name only instead of the full
    path (e.g. nvim instead of /usr/local/bin/nvim)

--- sidekicks/zsh/rc.d/config/environment.zsh
+++ sidekicks/zsh/rc.d/config/environment.zsh
@@ -111,7 +111,7 @@ export SPROMPT="zsh: correct %F{red}'%R'%f to %F{blue}'%r'%f [%B%Uy%u%bes, %B%Un
 #########
 
 # Set nvim as editor or use vim if nvim is not available
-(( $+commands[nvim] )) && export EDITOR=$commands[nvim] || export EDITOR=vim
+(( $+commands[nvim] )) && export EDITOR=nvim || export EDITOR=vim
 export VISUAL=$EDITOR
 export GIT_EDITOR=$EDITOR
 
