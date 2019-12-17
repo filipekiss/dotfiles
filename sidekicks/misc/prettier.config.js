b1cc4c7410cc8a04f07f3ae817e19c78617e4f82
Filipe Kiss <hello@filipekiss.com.br>
2019-12-17 10:38:33 +0100

    misc: Update prettier settings and editorconfig

--- sidekicks/misc/prettier.config.js
+++ sidekicks/misc/prettier.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-    useTabs: false, // Use spaces and not tabs
-    tabWidth: 4, // Ident using 4 spaces
-    semi: true, // Append semicolons to end of statement,
-    singleQuote: true, // Use single quote for strings,
-    trailingComma: 'es5', // Add trailing comma when valid. See https://prettier.io/docs/en/options.html#trailing-commas
-    bracketSpacing: false, // Change { foo: bar } to {foo: bar}
-    jsxBracketSameLine: false, // See https://prettier.io/docs/en/options.html#jsx-brackets
-    arrowParens: 'always', // For consistency. See https://prettier.io/docs/en/options.html#arrow-function-parentheses
-    proseWrap: 'always'
+	useTabs: true, // Use tabs by default
+	tabWidth: 4, // Ident using 4 spaces
+	semi: true, // Append semicolons to end of statement,
+	singleQuote: true, // Use single quote for strings,
+	trailingComma: 'es5', // Add trailing comma when valid. See https://prettier.io/docs/en/options.html#trailing-commas
+	bracketSpacing: false, // Change { foo: bar } to {foo: bar}
+	jsxBracketSameLine: false, // See https://prettier.io/docs/en/options.html#jsx-brackets
+	arrowParens: 'avoid', // For consistency. See https://prettier.io/docs/en/options.html#arrow-function-parentheses
+	proseWrap: 'always',
 };
